056d69754fd9afaab7c9098218b67d0cd5ebff87
Mario Behrendt <m.behrendt@yanted.com>
2016-11-04 16:40:05 +0000

    Version 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Filesize.Mixfile do
 
   def project do
     [app: :filesize,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
